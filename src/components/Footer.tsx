b975489513835ce4bf6add8d92ed96aa1946faa7
jneris-dev <site@jneris.com.br>
2022-10-21 16:16:07 -0300

    Footer Update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
     return (
         <footer className="w-full bg-main-500 h-auto py-4 px-4">
-            <div className="flex flex-row flex-wrap justify-between items-center mx-auto max-w-7xl gap-y-4 text-center">
+            <div className="flex flex-row flex-wrap lg:justify-between justify-center items-center mx-auto max-w-7xl gap-4 text-center">
                 <p className="text-sm text-white">
                     Dúvidas? Envie um e-mail para <a href="mail:atendimento@umbrindenaeuropa.com.br" className="text-[#653a23] hover:underline focus:underline underline-offset-4 focus:outline-none">atendimento@umbrindenaeuropa.com.br</a>
                 </p>
